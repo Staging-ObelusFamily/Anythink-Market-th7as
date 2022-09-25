d655e029128c7308c63ccd8594b2adefabdd5090
wilcoquests <110761906+wilcoquests@users.noreply.github.com>
2022-09-25 13:39:25 +0300

    Update CommentInput.js

--- frontend/src/components/Item/CommentInput.js
+++ frontend/src/components/Item/CommentInput.js
@@ -7,52 +7,33 @@ const mapDispatchToProps = (dispatch) => ({
   onSubmit: (payload) => dispatch({ type: ADD_COMMENT, payload }),
 });
 
-class CommentInput extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      body: "",
-    };
+function CommentInput({ currentUser }) {
+  const [body, setBody] = useState(");
 
-    this.setBody = (ev) => {
-      this.setState({ body: ev.target.value });
-    };
-
-    this.createComment = (ev) => {
-      ev.preventDefault();
-      const payload = agent.Comments.create(this.props.slug, {
-        body: this.state.body,
-      });
-      this.setState({ body: "" });
-      this.props.onSubmit(payload);
-    };
-  }
-
-  render() {
-    return (
-      <form className="card comment-form m-2" onSubmit={this.createComment}>
-        <div className="card-block">
-          <textarea
-            className="form-control"
-            placeholder="Write a comment..."
-            value={this.state.body}
-            onChange={this.setBody}
-            rows="3"
-          ></textarea>
-        </div>
-        <div className="card-footer">
-          <img
-            src={this.props.currentUser.image}
-            className="user-pic mr-2"
-            alt={this.props.currentUser.username}
-          />
-          <button className="btn btn-sm btn-primary" type="submit">
-            Post Comment
-          </button>
-        </div>
-      </form>
-    );
-  }
+  return (
+    <form className="card comment-form m-2" onSubmit={}>
+      <div className="card-block">
+        <textarea
+          className="form-control"
+          placeholder="Write a comment..."
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+          rows="3"
+        ></textarea>
+      </div>
+      <div className="card-footer">
+        <img
+          src={currentUser.image}
+          className="user-pic mr-2"
+          alt={
+            currentUser.username}
+        />
+        <button className="btn btn-sm btn-primary" type="submit">
+          Post Comment
+        </button>
+      </div>
+    </form>
+  )
 }
 
 export default connect(() => ({}), mapDispatchToProps)(CommentInput);
